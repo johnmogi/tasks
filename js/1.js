26e76842a81a3b1b191c904c94d13c4fb37d1b65
johnmogi <anguru@gmail.com>
2021-01-02 19:13:06 +0200

    timeValidation

--- js/1.js
+++ js/1.js
@@ -1,13 +1,18 @@
 const taskForm = document.getElementById('taskForm');
 const taskName = document.getElementById('taskName');
 const taskDesc = document.getElementById('taskDesc');
+const taskDueDate = document.getElementById('taskDueDate');
+const taskBoard = document.getElementById('taskBoard');
+
 let formValid = false;
 
 function addTask(){
-    let valArr = [taskName,taskDesc]
+    let valArr = [taskName,taskDesc,taskDueDate]
     for (let i = 0; i < valArr.length; i++) {
-       
         validate(valArr[i])     
+    }
+    if(!taskDueDate) {
+        return false
     }
      formValid ? sendForm(valArr) : null;
 }
@@ -24,9 +29,34 @@ function validate(a){
     }
     
 }
+function drawCard(arr){
 
+    const divCard = document.createElement("div");
+    divCard.className = "card column is-one-quarter";
+    const divCardContent = document.createElement("div");
+    divCardContent.className = "card-content";
+    const mediaCard = document.createElement("div");
+    mediaCard.className = "media-content";
+    const titleCard = document.createElement("p");
+    titleCard.className = "title is-4";
+    const divCardContentInner = document.createElement("div");
+    divCardContentInner.className = "content";
+    const br = document.createElement("br");
+    const timeCard = document.createElement("time");
+    timeCard.datetime = arr[1].value;
+    timeCard.innerHTML= arr[1].value;
+    titleCard.innerHTML= arr[1].value;
+    divCardContentInner.innerHTML= arr[0].value;
+    divCardContentInner.appendChild(br);
+    divCardContentInner.appendChild(timeCard);
+    mediaCard.appendChild(titleCard);
+    divCardContent.appendChild(mediaCard);
+    divCard.appendChild(divCardContentInner);
+    divCard.appendChild(divCardContent);
+    taskBoard.appendChild(divCard)
+}
 function sendForm(arr){
-    console.log('arr', arr)
+    drawCard(arr)
 }
 // prevent page from reloading after submit
 document.getElementById("taskForm").addEventListener("click", function(event){
